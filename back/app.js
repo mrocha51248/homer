ddbdf26131c2ed6f4d0e5f3bd43875609736d525
mrocha51248 <87133024+mrocha51248@users.noreply.github.com>
2022-02-21 11:36:38 +0100

    Back: Create /auth/signup route

--- back/app.js
+++ back/app.js
@@ -5,15 +5,18 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 
 const app = express();
+const authRouter = require('./routes/auth/auth');
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
     res.send("youhou");
 })
+
+app.use('/auth', authRouter);
 
 app.use(function (req, res, next) {
     var err = new Error('Not Found');
