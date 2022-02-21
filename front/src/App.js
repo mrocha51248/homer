87c68c95002b7c008d1ef58e8d14eda5e562a20d
mrocha51248 <87133024+mrocha51248@users.noreply.github.com>
2022-02-21 18:35:35 +0100

    Front: Update App layout

--- front/src/App.js
+++ front/src/App.js
@@ -1,12 +1,26 @@
+import { Grid, Paper } from "@mui/material";
 import SignUp from "./components/SignUp";
 
 import './App.css';
 
 function App() {
   return (
-    <div>
-      <SignUp email="hello@homer.com"/>
-    </div>
+    <Grid container alignItems='center' style={{ height: '100%' }}>
+      <Grid item xs={12}>
+        <Paper elevation={4} style={{ margin: 32 }}>
+          <Grid container
+            alignItems='center'
+            justify='center'>
+            <Grid item xs={12} sm={6} style={{ textAlign: 'center' }}>
+              <img src="/assets/images/wildhomer.png" />
+            </Grid>
+            <Grid item xs={6} alignContent='center'>
+              <SignUp />
+            </Grid>
+          </Grid>
+        </Paper>
+      </Grid>
+    </Grid>
   );
 }
 
