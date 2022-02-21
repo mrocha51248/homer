d0ecc57753e1e0c92d8c037965a7695714e61492
mrocha51248 <87133024+mrocha51248@users.noreply.github.com>
2022-02-21 14:16:40 +0100

    Back: Implement /auth/signup route

--- back/routes/auth/auth.js
+++ back/routes/auth/auth.js
@@ -1,9 +1,16 @@
 const express = require('express');
 
 const router = express.Router();
+const connection = require('../../helpers/db');
 
 router.post('/signup', function (req, res, next) {
-    res.send('I am in POST signup');
+    connection.query({
+        sql: 'INSERT INTO users (email, password, firstname, lastname) VALUES (?, ?, ?, ?)',
+        values: [req.body.email, req.body.password, req.body.firstName, req.body.lastName],
+    }, function (error, results, fields) {
+        if (error) res.status(500).end();
+        res.end();
+    });
 });
 
 module.exports = router;
