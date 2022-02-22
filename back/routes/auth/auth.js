e71efb61b96b2e017957e57feb30ee3fe3422a7f
mrocha51248 <87133024+mrocha51248@users.noreply.github.com>
2022-02-22 15:25:05 +0100

    Back: auth.js: hash password in /signup

--- back/routes/auth/auth.js
+++ back/routes/auth/auth.js
@@ -1,12 +1,14 @@
+const bcrypt = require('bcrypt');
 const express = require('express');
 
 const router = express.Router();
 const connection = require('../../helpers/db');
 
 router.post('/signup', function (req, res, next) {
+    const passwordHash = bcrypt.hashSync(req.body.password, 10);
     connection.query({
         sql: 'INSERT INTO users (email, password, firstname, lastname) VALUES (?, ?, ?, ?)',
-        values: [req.body.email, req.body.password, req.body.firstName, req.body.lastName],
+        values: [req.body.email, passwordHash, req.body.firstName, req.body.lastName],
     }, function (error, results, fields) {
         if (error)
             res.status(500).json({ flash: error.message });
