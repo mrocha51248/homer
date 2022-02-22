566961dd921249bdee5521c67afb3512a21a6034
mrocha51248 <87133024+mrocha51248@users.noreply.github.com>
2022-02-22 15:11:19 +0100

    Front: Create Routes in App

--- front/src/App.js
+++ front/src/App.js
@@ -1,4 +1,7 @@
 import { Grid, Paper } from "@mui/material";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Profile from "./components/Profile";
+import SignIn from "./components/SignIn";
 import SignUp from "./components/SignUp";
 
 import './App.css';
@@ -14,8 +17,15 @@ function App() {
             <Grid item xs={12} sm={6} style={{ textAlign: 'center' }}>
               <img src="/assets/images/wildhomer.png" />
             </Grid>
-            <Grid item xs={6} alignContent='center'>
-              <SignUp />
+            <Grid item xs={12} sm={6} alignContent='center'>
+              <BrowserRouter>
+                <Routes>
+                  <Route exact path="/" element={<SignIn />} />
+                  <Route path="/signup" element={<SignUp />} />
+                  <Route path="/signin" element={<SignIn />} />
+                  <Route path="/profile" element={<Profile />} />
+                </Routes>
+              </BrowserRouter>
             </Grid>
           </Grid>
         </Paper>
